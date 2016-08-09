a944a848d2b54083fb40ea78b13291a8ceb66b83
Jokeren <robinho364@gmail.com>
2016-08-09 19:24:23 +0800

    remove backup files

--- src/backup/backend/backends.h
+++
@@ -1,9 +0,0 @@
-#ifndef SRC_BACKEND_BACKENDS_H_
-#define SRC_BACKEND_BACKENDS_H_
-
-#include "backend/cpu_backend.h"
-#include "backend/cpu_tensor.h"
-#include "backend/gpu_backend.h"
-#include "backend/gpu_tensor.h"
-
-#endif  // SRC_BACKEND_BACKENDS_H_
