94adfe3507506eece8bb8c7e20ccd7b2ed5912e2
Jokeren <robinho364@gmail.com>
2016-10-05 18:16:40 +0800

    pool fallback for compatability

--- src/backend/cpu_backend_pool-inl.h
+++ src/backend/cpu_backend_pool-inl.h
@@ -11,27 +11,39 @@ void Backend<CPUTensor, DType>::MaxPooling2DForwardFunc(
   // input
   const Shape& input_shape = input->shape();
   const size_t batch_size = input_shape[0];
-  const size_t channel = input_shape[1];
+  const size_t input_channel = input_shape[1];
   const size_t input_height = input_shape[2];
   const size_t input_width = input_shape[3];
   // output
   const Shape& output_shape = output->shape();
+  const size_t output_channel = output_shape[1];
   const size_t output_height = output_shape[2];
   const size_t output_width = output_shape[3];
-  // multi-dimensional cast
-  const DType (&p_input)[batch_size][channel][input_height * input_width] = 
-    *reinterpret_cast<const DType (*)[batch_size][channel]
-    [input_height * input_width]>(input->data());
-  DType (&p_output)[batch_size][channel][output_height * output_width] =
-    *reinterpret_cast<DType (*)[batch_size][channel]
-    [output_height * output_width]>(output->data());
-  size_t (&p_max_index)[batch_size][channel][output_height * output_width] =
-    *reinterpret_cast<size_t (*)[batch_size][channel]
-    [output_height * output_width]>(max_index->data());
+  // offset
+  const size_t input_single_size = input_channel * input_height * input_width;
+  const size_t input_channel_size = input_height * input_width;
+  const size_t output_single_size = output_channel * output_height * output_width;
+  const size_t output_channel_size = output_height * output_width;
+  size_t input_channel_offset;
+  size_t output_channel_offset;
+  size_t input_batch_offset;
+  size_t output_batch_offset;
+  CHECK_EQ(input_channel, output_channel);
   // no padding
-  #pragma omp parallel for
+  #pragma omp parallel for private(input_batch_offset, output_batch_offset, \
+    input_channel_offset, output_channel_offset)
   for (size_t batch_index = 0; batch_index < batch_size; ++batch_index) {
-    for (size_t channel_index = 0; channel_index < channel; ++channel_index) {
+    input_batch_offset = batch_index * input_single_size;
+    output_batch_offset = batch_index * output_single_size;
+    for (size_t channel_index = 0; channel_index < input_channel; ++channel_index) {
+      input_channel_offset = channel_index * input_channel_size;
+      output_channel_offset = channel_index * output_channel_size;
+      const DType* input_slice = input->Slice(input_batch_offset +
+        input_channel_offset);
+      DType* output_slice = output->Slice(output_batch_offset +
+        output_channel_offset);
+      size_t* max_index_slice = max_index->Slice(output_batch_offset +
+        output_channel_offset);
       for (size_t output_height_index = 0; output_height_index < output_height;
         ++output_height_index) {
         for (size_t output_width_index = 0; output_width_index < output_width;
@@ -40,21 +52,19 @@ void Backend<CPUTensor, DType>::MaxPooling2DForwardFunc(
           size_t width_start = output_width_index * stride_width;
           size_t height_end = height_start + filter_height;
           size_t width_end = width_start + filter_width;
+          size_t pool_index = output_height_index * output_width +
+            output_width_index;
           size_t max_index_tmp = height_start * input_width + width_start;
           for (size_t h = height_start; h < height_end; ++h) {
             for (size_t w = width_start; w < width_end; ++w) {
-              const size_t index = h * input_width + w;
-              if (p_input[batch_index][channel_index][index] >
-                  p_input[batch_index][channel_index][max_index_tmp]) {
+              size_t index = h * input_width + w;
+              if (input_slice[index] > input_slice[max_index_tmp]) {
                 max_index_tmp = index;
               }
             }
           }
-          const size_t pool_index = output_height_index * output_width +
-            output_width_index;
-          p_output[batch_index][channel_index][pool_index] =
-            p_input[batch_index][channel_index][max_index_tmp];
-          p_max_index[batch_index][channel_index][pool_index] = max_index_tmp;
+          output_slice[pool_index] = input_slice[max_index_tmp];
+          max_index_slice[pool_index] = max_index_tmp;
         }
       }
     }
@@ -78,31 +88,39 @@ void Backend<CPUTensor, DType>::MaxPooling2DBackwardFunc(
   const Shape& output_shape = output->shape();
   const size_t output_height = output_shape[2];
   const size_t output_width = output_shape[3];
-  // multi-dimensional cast
-  DType (&p_input)[batch_size][channel][input_height * input_width] = 
-    *reinterpret_cast<DType (*)[batch_size][channel]
-    [input_height * input_width]>(input->data());
-  const DType (&p_output)[batch_size][channel][output_height * output_width] =
-    *reinterpret_cast<const DType (*)[batch_size][channel]
-    [output_height * output_width]>(output->data());
-  const size_t (&p_max_index)[batch_size][channel][output_height * output_width] =
-    *reinterpret_cast<const size_t (*)[batch_size][channel]
-    [output_height * output_width]>(max_index->data());
+  // offset
+  const size_t input_single_size = channel * input_height * input_width;
+  const size_t input_channel_size = input_height * input_width;
+  const size_t output_single_size = channel * output_height * output_width;
+  const size_t output_channel_size = output_height * output_width;
+  size_t input_batch_offset;
+  size_t output_batch_offset;
+  size_t input_channel_offset;
+  size_t output_channel_offset;
   // set zero
   input->Fill(0);
   // no padding
-  #pragma omp parallel for
+  #pragma omp parallel for private(input_batch_offset, output_batch_offset, \
+    input_channel_offset, output_channel_offset)
   for (size_t batch_index = 0; batch_index < batch_size; ++batch_index) {
+    input_batch_offset = batch_index * input_single_size;
+    output_batch_offset = batch_index * output_single_size;
     for (size_t channel_index = 0; channel_index < channel; ++channel_index) {
+      input_channel_offset = channel_index * input_channel_size;
+      output_channel_offset = channel_index * output_channel_size;
+      DType* input_slice = input->Slice(input_batch_offset +
+        input_channel_offset);
+      const DType* output_slice = output->Slice(output_batch_offset +
+        output_channel_offset);
+      const size_t* max_index_slice = max_index->Slice(output_batch_offset +
+        output_channel_offset);
       for (size_t output_height_index = 0; output_height_index < output_height;
         ++output_height_index) {
         for (size_t output_width_index = 0; output_width_index < output_width;
           ++output_width_index) {
-          const size_t index = p_max_index[batch_index][channel_index]
-            [output_height_index * output_width + output_width_index];
-          p_input[batch_index][channel_index][index] =
-            p_output[batch_index][channel_index]
-            [output_height_index * output_width + output_width_index];
+          size_t index = output_height_index * output_width +
+            output_width_index;
+          input_slice[max_index_slice[index]] = output_slice[index];
         }
       }
     }
