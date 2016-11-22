8e49a45a2f83cf954f4b3ab583c2b7edeb22f313
Jokeren <robinho364@gmail.com>
2016-11-22 20:43:37 +0800

    cpu pack optimization

--- scripts/samples/run_convolution_experiment.sh
+++ scripts/samples/run_convolution_experiment.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#phase N C H W R S K P Q pad_h pad_w str_h str_w
+#phase N C H W R S K P Q pad_h pad_w str_h str_w iterations
 
 #N
 #./samples/gpu/convolution/convolution forward convolution_sass_direct 128 256 24 24 4 4 256 21 21 0 0 1 1 2
@@ -30,14 +30,14 @@
 #./samples/gpu/convolution/convolution forward convolution_sass_direct 128 256 72 72 4 4 256 69 69 0 0 1 1 2
 
 #filter size
-#./samples/gpu/convolution/convolution forward convolution_sass_direct 128 256 24 24 16 16 128 9 9 0 0 1 1 2
-#./samples/gpu/convolution/convolution forward convolution_sass_direct 128 256 24 24 14 14 128 11 11 0 0 1 1 2
-#./samples/gpu/convolution/convolution forward convolution_sass_direct 128 256 24 24 12 12 128 13 13 0 0 1 1 2
-#./samples/gpu/convolution/convolution forward convolution_sass_direct 128 256 24 24 10 10 128 15 15 0 0 1 1 2
-#./samples/gpu/convolution/convolution forward convolution_sass_direct 128 256 24 24 8 8 128 17 17 0 0 1 1 2
-#./samples/gpu/convolution/convolution forward convolution_sass_direct 128 256 24 24 6 6 128 19 19 0 0 1 1 2
-#./samples/gpu/convolution/convolution forward convolution_sass_direct 128 256 24 24 4 4 128 21 21 0 0 1 1 2
-#./samples/gpu/convolution/convolution forward convolution_sass_direct 128 256 24 24 2 2 128 23 23 0 0 1 1 2
+./samples/gpu/convolution/convolution forward convolution_sass_direct 128 256 24 24 16 16 256 9 9 0 0 1 1 2
+./samples/gpu/convolution/convolution forward convolution_sass_direct 128 256 24 24 14 14 256 11 11 0 0 1 1 2
+./samples/gpu/convolution/convolution forward convolution_sass_direct 128 256 24 24 12 12 256 13 13 0 0 1 1 2
+./samples/gpu/convolution/convolution forward convolution_sass_direct 128 256 24 24 10 10 256 15 15 0 0 1 1 2
+./samples/gpu/convolution/convolution forward convolution_sass_direct 128 256 24 24 8 8 256 17 17 0 0 1 1 2
+./samples/gpu/convolution/convolution forward convolution_sass_direct 128 256 24 24 6 6 256 19 19 0 0 1 1 2
+./samples/gpu/convolution/convolution forward convolution_sass_direct 128 256 24 24 4 4 256 21 21 0 0 1 1 2
+./samples/gpu/convolution/convolution forward convolution_sass_direct 128 256 24 24 2 2 256 23 23 0 0 1 1 2
 
 #K
 #./samples/gpu/convolution/convolution forward convolution_sass_direct 128 256 24 24 4 4 128 21 21 0 0 1 1 2
@@ -97,14 +97,14 @@
 #./samples/gpu/convolution/cudnn forward gemm_pre 128 256 72 72 4 4 256 69 69 0 0 1 1 2
 
 #filter size
-#./samples/gpu/convolution/cudnn forward gemm_pre 128 256 24 24 16 16 128 9 9 0 0 1 1 2
-#./samples/gpu/convolution/cudnn forward gemm_pre 128 256 24 24 14 14 128 11 11 0 0 1 1 2
-#./samples/gpu/convolution/cudnn forward gemm_pre 128 256 24 24 12 12 128 13 13 0 0 1 1 2
-#./samples/gpu/convolution/cudnn forward gemm_pre 128 256 24 24 10 10 128 15 15 0 0 1 1 2
-#./samples/gpu/convolution/cudnn forward gemm_pre 128 256 24 24 8 8 128 17 17 0 0 1 1 2
-#./samples/gpu/convolution/cudnn forward gemm_pre 128 256 24 24 6 6 128 19 19 0 0 1 1 2
-#./samples/gpu/convolution/cudnn forward gemm_pre 128 256 24 24 4 4 128 21 21 0 0 1 1 2
-#./samples/gpu/convolution/cudnn forward gemm_pre 128 256 24 24 2 2 128 23 23 0 0 1 1 2
+#./samples/gpu/convolution/cudnn forward gemm_pre 128 256 24 24 16 16 256 9 9 0 0 1 1 2
+#./samples/gpu/convolution/cudnn forward gemm_pre 128 256 24 24 14 14 256 11 11 0 0 1 1 2
+#./samples/gpu/convolution/cudnn forward gemm_pre 128 256 24 24 12 12 256 13 13 0 0 1 1 2
+#./samples/gpu/convolution/cudnn forward gemm_pre 128 256 24 24 10 10 256 15 15 0 0 1 1 2
+#./samples/gpu/convolution/cudnn forward gemm_pre 128 256 24 24 8 8 256 17 17 0 0 1 1 2
+#./samples/gpu/convolution/cudnn forward gemm_pre 128 256 24 24 6 6 256 19 19 0 0 1 1 2
+#./samples/gpu/convolution/cudnn forward gemm_pre 128 256 24 24 4 4 256 21 21 0 0 1 1 2
+#./samples/gpu/convolution/cudnn forward gemm_pre 128 256 24 24 2 2 256 23 23 0 0 1 1 2
 
 #K
 #./samples/gpu/convolution/cudnn forward gemm_pre 128 256 24 24 4 4 128 21 21 0 0 1 1 2
@@ -125,11 +125,11 @@
 #./samples/gpu/convolution/cudnn forward gemm_pre 128 256 24 24 4 4 128 4 4 0 0 6 6 2
 
 #pad
-./samples/gpu/convolution/cudnn forward gemm_implicit 128 256 24 24 8 8 128 17 17 0 0 1 1 2
-./samples/gpu/convolution/cudnn forward gemm_implicit 128 256 24 24 8 8 128 19 19 1 1 1 1 2
-./samples/gpu/convolution/cudnn forward gemm_implicit 128 256 24 24 8 8 128 21 21 2 2 1 1 2
-./samples/gpu/convolution/cudnn forward gemm_implicit 128 256 24 24 8 8 128 23 23 3 3 1 1 2
-./samples/gpu/convolution/cudnn forward gemm_implicit 128 256 24 24 8 8 128 25 25 4 4 1 1 2
-./samples/gpu/convolution/cudnn forward gemm_implicit 128 256 24 24 8 8 128 27 27 5 5 1 1 2
-./samples/gpu/convolution/cudnn forward gemm_implicit 128 256 24 24 8 8 128 29 29 6 6 1 1 2
-./samples/gpu/convolution/cudnn forward gemm_implicit 128 256 24 24 8 8 128 31 31 7 7 1 1 2
+#./samples/gpu/convolution/cudnn forward gemm_implicit 128 256 24 24 8 8 128 17 17 0 0 1 1 2
+#./samples/gpu/convolution/cudnn forward gemm_implicit 128 256 24 24 8 8 128 19 19 1 1 1 1 2
+#./samples/gpu/convolution/cudnn forward gemm_implicit 128 256 24 24 8 8 128 21 21 2 2 1 1 2
+#./samples/gpu/convolution/cudnn forward gemm_implicit 128 256 24 24 8 8 128 23 23 3 3 1 1 2
+#./samples/gpu/convolution/cudnn forward gemm_implicit 128 256 24 24 8 8 128 25 25 4 4 1 1 2
+#./samples/gpu/convolution/cudnn forward gemm_implicit 128 256 24 24 8 8 128 27 27 5 5 1 1 2
+#./samples/gpu/convolution/cudnn forward gemm_implicit 128 256 24 24 8 8 128 29 29 6 6 1 1 2
+#./samples/gpu/convolution/cudnn forward gemm_implicit 128 256 24 24 8 8 128 31 31 7 7 1 1 2
