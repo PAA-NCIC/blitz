1f29cd5aaa9273d3bfb4e8dd97e22d9be356e28a
Jokeren <robinho364@gmail.com>
2016-12-20 13:04:30 +0800

    Add kernel select

--- samples/gpu/matrix/matrix_multiply.cc
+++ samples/gpu/matrix/matrix_multiply.cc
@@ -23,7 +23,7 @@ void compare_cpu_gpu(size_t size, float* output_cpu, float* output_gpu) {
   }
 }
 
-void multiply(size_t m, size_t n, size_t k) {
+void multiply(size_t m, size_t n, size_t k, BLITZ_ALGORITHM algorithm) {
   // set up cpu
   CPUTensor<float> left_cpu(left_shape);
   CPUTensor<float> right_cpu(right_shape);
@@ -41,24 +41,25 @@ void multiply(size_t m, size_t n, size_t k) {
   cudaMemcpy(right_gpu.data(), right_cpu.data(),
     right_cpu.size() * sizeof(float), cudaMemcpyHostToDevice);
   // transpose
-  Backend<CPUTensor, float>::MatrixMultiplyFunc(&left_cpu, &right_cpu, &output_cpu, false, false, 1.0, 0.0);
-  Backend<GPUTensor, float>::MatrixMultiplyFunc(&left_gpu, &right_gpu, &output_gpu, false, false, 1.0, 0.0);
+  Backend<CPUTensor, float>::MatrixMultiplyFunc(&left_cpu, &right_cpu, &output_cpu, true, false, 1.0, 0.0, algorithm);
+  Backend<GPUTensor, float>::MatrixMultiplyFunc(&left_gpu, &right_gpu, &output_gpu, true, false, 1.0, 0.0, algorithm);
   // copy from gpu to cpu
   cudaMemcpy(output_copy.data(), output_gpu.data(),
     output_gpu.size() * sizeof(float), cudaMemcpyDeviceToHost);
-  compare_cpu_gpu(output_cpu.size(), output_cpu.data(), output_copy.data());
+  //compare_cpu_gpu(output_cpu.size(), output_cpu.data(), output_copy.data());
 }
 
 int main(int argc, char** argv) {
-  const size_t NUM_ARGS = 3;
+  const size_t NUM_ARGS = 4;
   // M N K
   if (argc != NUM_ARGS + 1) {
     std::cerr << "Not enough args!" << std::endl;
     exit(1);
   }
-  const size_t M = atoi(argv[1]);
-  const size_t N = atoi(argv[2]);
-  const size_t K = atoi(argv[3]);
+	std::string kernel = std::string(argv[1]);
+  const size_t M = atoi(argv[2]);
+  const size_t N = atoi(argv[3]);
+  const size_t K = atoi(argv[4]);
   // set shapes
   left_shape[0] = M;
   left_shape[1] = K;
@@ -67,6 +68,6 @@ int main(int argc, char** argv) {
   output_shape[0] = M;
   output_shape[1] = N;
   // run
-  multiply(M, N, K);
+  multiply(M, N, K, BlitzParseAlgorithm(kernel));
   return 0;
 }
