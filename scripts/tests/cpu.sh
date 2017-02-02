612b9e533ae1fe687fb2e669fee7bc30586fd4f5
Jokeren <robinho364@gmail.com>
2017-02-02 22:15:35 +0800

    modify travis

--- scripts/tests/cpu.sh
+++ scripts/tests/cpu.sh
@@ -16,41 +16,41 @@ for((v=0;v<${#OUTPUT_LAYOUT[@]};v++))
 do
 if ./samples/cpu/convolution/convolution ${PHASE[$i]} ${ALG[$j]} ${INPUT_LAYOUT[$k]} ${OUTPUT_LAYOUT[$v]} 128 3 224 224 11 11 64 55 55 3 3 4 4 1
 then
-	echo "Alexnet first layer pass!"
+	echo "Alexnet first layer pass!" ${PHASE[$i]} ${ALG[$j]} ${INPUT_LAYOUT[$k]} ${OUTPUT_LAYOUT[$v]}
 else
-	echo "Alexnet first layer fail!"
+	echo "Alexnet first layer fail!" ${PHASE[$i]} ${ALG[$j]} ${INPUT_LAYOUT[$k]} ${OUTPUT_LAYOUT[$v]}
 	exit 1
 fi
 
 if ./samples/cpu/convolution/convolution ${PHASE[$i]} ${ALG[$j]} ${INPUT_LAYOUT[$k]} ${OUTPUT_LAYOUT[$v]} 128 64 27 27 5 5 192 27 27 2 2 1 1 1
 then
-	echo "Alexnet second layer pass!"
+	echo "Alexnet second layer pass!" ${PHASE[$i]} ${ALG[$j]} ${INPUT_LAYOUT[$k]} ${OUTPUT_LAYOUT[$v]}
 else
-	echo "Alexnet second layer fail!"
+	echo "Alexnet second layer fail!" ${PHASE[$i]} ${ALG[$j]} ${INPUT_LAYOUT[$k]} ${OUTPUT_LAYOUT[$v]}
 	exit 1
 fi
 
 if ./samples/cpu/convolution/convolution ${PHASE[$i]} ${ALG[$j]} ${INPUT_LAYOUT[$k]} ${OUTPUT_LAYOUT[$v]} 128 192 13 13 3 3 384 13 13 1 1 1 1 1 
 then
-	echo "Alexnet third layer pass!"
+	echo "Alexnet third layer pass!" ${PHASE[$i]} ${ALG[$j]} ${INPUT_LAYOUT[$k]} ${OUTPUT_LAYOUT[$v]}
 else
-	echo "Alexnet third layer fail!"
+	echo "Alexnet third layer fail!" ${PHASE[$i]} ${ALG[$j]} ${INPUT_LAYOUT[$k]} ${OUTPUT_LAYOUT[$v]}
 	exit 1
 fi
 
 if ./samples/cpu/convolution/convolution ${PHASE[$i]} ${ALG[$j]} ${INPUT_LAYOUT[$k]} ${OUTPUT_LAYOUT[$v]} 128 384 13 13 3 3 256 13 13 1 1 1 1 1
 then
-	echo "Alexnet fourth layer pass!"
+	echo "Alexnet fourth layer pass!" ${PHASE[$i]} ${ALG[$j]} ${INPUT_LAYOUT[$k]} ${OUTPUT_LAYOUT[$v]}
 else
-	echo "Alexnet fourth layer fail!"
+	echo "Alexnet fourth layer fail!" ${PHASE[$i]} ${ALG[$j]} ${INPUT_LAYOUT[$k]} ${OUTPUT_LAYOUT[$v]}
 	exit 1
 fi
 
 if ./samples/cpu/convolution/convolution ${PHASE[$i]} ${ALG[$j]} ${INPUT_LAYOUT[$k]} ${OUTPUT_LAYOUT[$v]} 128 256 13 13 3 3 256 13 13 1 1 1 1 1
 then
-	echo "Alexnet fifth layer pass!"
+	echo "Alexnet fifth layer pass!" ${PHASE[$i]} ${ALG[$j]} ${INPUT_LAYOUT[$k]} ${OUTPUT_LAYOUT[$v]}
 else
-	echo "Alexnet fifth layer fail!"
+	echo "Alexnet fifth layer fail!" ${PHASE[$i]} ${ALG[$j]} ${INPUT_LAYOUT[$k]} ${OUTPUT_LAYOUT[$v]}
 	exit 1
 fi
 done
