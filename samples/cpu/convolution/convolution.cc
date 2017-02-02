d96978e04359a8bae18ce230fba74e0e80d12fe8
Jokeren <robinho364@gmail.com>
2017-02-03 00:07:37 +0800

    Remove BLITZ_NUM_THREADS

--- samples/cpu/convolution/convolution.cc
+++ samples/cpu/convolution/convolution.cc
@@ -88,7 +88,7 @@ void convolution_forward(
   CPUTensor<float> output_cpu(output_shape);
   CPUTensor<float> output_cpu_algorithm(output_shape);
   CPUTensor<float> workspace_cpu(workspace_shape_cpu);
-  size_t workspace_size = workspace_shape_cpu.size() * omp_get_num_threads();
+  size_t workspace_size = workspace_shape_cpu.size() * omp_get_max_threads();
   Shape workspace_shape_algorithm(1);
   workspace_shape_algorithm[0] = workspace_size;
   CPUTensor<float> workspace_cpu_algorithm(workspace_shape_algorithm);
@@ -128,7 +128,7 @@ void convolution_backward(
   CPUTensor<float> filter_cpu(filter_shape);
   CPUTensor<float> output_cpu(output_shape);
   CPUTensor<float> workspace_cpu(workspace_shape_cpu);
-  size_t workspace_size = workspace_shape_cpu.size() * omp_get_num_threads();
+  size_t workspace_size = workspace_shape_cpu.size() * omp_get_max_threads();
   Shape workspace_shape_algorithm(1);
   workspace_shape_algorithm[0] = workspace_size;
   CPUTensor<float> workspace_cpu_algorithm(workspace_shape_algorithm);
@@ -168,7 +168,7 @@ void convolution_update(
   CPUTensor<float> filter_cpu_algorithm(filter_shape);
   CPUTensor<float> output_cpu(output_shape);
   CPUTensor<float> workspace_cpu(workspace_shape_cpu);
-  size_t workspace_size = workspace_shape_cpu.size() * omp_get_num_threads();
+  size_t workspace_size = workspace_shape_cpu.size() * omp_get_max_threads();
   Shape workspace_shape_algorithm(1);
   workspace_shape_algorithm[0] = workspace_size;
   CPUTensor<float> workspace_cpu_algorithm(workspace_shape_algorithm);
